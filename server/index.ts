fd713b9a95f452fe546002c5a4213582ee4540d4
davidrepository <dawidxpluciennik@gmail.com>
2025-12-16 03:15:35 +0100

    feat(server): initial Express server setup

---
+++ server/index.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const PORT: number = 4000;
+
+app.listen(PORT, () => {
+  console.log(`SERVER: http://localhost:${PORT}`);
+});
